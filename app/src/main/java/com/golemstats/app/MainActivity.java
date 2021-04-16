2b83a39906aafe597c9fa8aa363b103a5f5410f3
JGTabb <jonasbellaert@outlook.com>
2021-04-16 17:01:06 +0200

    Main menu update 2

--- app/src/main/java/com/golemstats/app/MainActivity.java
+++ app/src/main/java/com/golemstats/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.golemstats.app;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -22,9 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         //hooks
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
         //toolbar
-
+        setSupportActionBar(toolbar);
         //navigation drawer menu
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
