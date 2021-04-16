a730ba53c051dfba29ec0de7bfad756d2ea3e718
JGTabb <jonasbellaert@outlook.com>
2021-04-16 12:03:03 +0200

    Golem Stats Android App Commit 2

--- app/src/main/java/com/golemstats/app/MainActivity.java
+++ app/src/main/java/com/golemstats/app/MainActivity.java
@@ -2,15 +2,30 @@ package com.golemstats.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    //variables
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        //toolbar
+
+        //navigation drawer menu
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
